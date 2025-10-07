b32170daa2c2ba691e78f9fc8a6a7048a2994879
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2025-10-07 16:47:10 +0800

    Update: createBlogPost.js

--- Scripts/Templater/createBlogPost.js
+++ Scripts/Templater/createBlogPost.js
@@ -65,73 +65,10 @@ async function createBlogPost(tp) {
         await tp.file.move(folder + fileName);
     }
 
-    const selectedTags = [];
-    if (blog !== "Notes-to-Self") {
-        const allTags = Object.keys(app.metadataCache.getTags());
-        const blogTags = allTags
-            .filter((tag) => tag.startsWith(`#${blog}/`))
-            .map((tag) => tag.substring(1));
-
-        let availableTags = [...blogTags];
-
-        while (selectedTags.length < 3) {
-            const displayChoices = [
-                "✅ Done",
-                "🪧 Create new tag",
-                ...availableTags.map(
-                    (tag) => `🏷️ ${tag.replace(`${blog}/`, "")}`
-                ),
-            ];
-            const valueChoices = [
-                "✅ Done",
-                "🪧 Create new tag",
-                ...availableTags,
-            ];
-            const selectedTagsText =
-                selectedTags.length > 0
-                    ? " " +
-                      `[${selectedTags
-                          .map((tag) =>
-                              tp.user.slugify(tag.replace(`${blog}/`, ""))
-                          )
-                          .join(", ")}]`
-                    : "";
-            const selectedChoice = await tp.system.suggester(
-                displayChoices,
-                valueChoices,
-                false,
-                "🤖 Which one?" + selectedTagsText
-            );
-
-            if (!selectedChoice || selectedChoice === "✅ Done") break;
-
-            if (selectedChoice === "🪧 Create new tag") {
-                const newTag = await tp.system.prompt("New tag?");
-                const fullTagName = newTag.trim().startsWith(`${blog}/`)
-                    ? newTag.trim()
-                    : `${blog}/${newTag.trim()}`;
-                selectedTags.push(fullTagName);
-            } else {
-                selectedTags.push(selectedChoice);
-                availableTags = availableTags.filter(
-                    (tag) => tag !== selectedChoice
-                );
-            }
-        }
-    }
-
-    const tags =
-        selectedTags.length > 0
-            ? selectedTags.map((tag) => {
-                  const parts = tag.split("/");
-                  return `${parts[0]}/${tp.user.slugify(parts[1])}`;
-              })
-            : [`${blog}/`];
-
     return {
         title,
+        fileName,
         blog,
-        tags,
     };
 }
 
