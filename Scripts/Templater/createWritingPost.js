702a51b36774403f934b9398d7e147d116dcc3d5
Hua-Ming Huang <huaming.huang.tw@gmail.com>
2026-03-09 19:05:40 +0800

    Update: createWritingPost.js

--- Scripts/Templater/createWritingPost.js
+++ Scripts/Templater/createWritingPost.js
@@ -42,7 +42,7 @@ async function createWritingPost(tp) {
         const userInput = await tp.system.prompt("📁 Filename?");
         rawFileName = userInput || temp;
     } else if (collection === "Enoughness") {
-        rawFileName = (moment().day() <= 5 ? moment().day(5) : moment().add(1, 'week').day(5)).format("YYYY[-week-]ww");
+        rawFileName = tp.user.upcomingFriday().format("YYYY[-week-]ww");
     } else {
         rawFileName = title;
     }
